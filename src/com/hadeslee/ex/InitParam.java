9025c158479e55f2dc437f88b32b63a4b4ed635e
hadaslee <leesk55@gmail.com>
2015-05-08 10:39:55 +0900

    서블릿 초기 파라미터 :servletConfig InitParam 서블릿에서 적용

--- src/com/hadeslee/ex/InitParam.java
+++ src/com/hadeslee/ex/InitParam.java
@@ -2,6 +2,7 @@ package com.hadeslee.ex;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -18,7 +19,8 @@ import java.io.PrintWriter;
  * Note:
  * To change this template use File | Settings | File Templates.
  */
-//@WebServlet(name = "InitParam")
+@WebServlet(urlPatterns = {"/InitP"}, initParams = {@WebInitParam(name = "id", value = "aaaa"), @WebInitParam(name = "pw", value = "99999"),@WebInitParam(name = "path", value = "C:\\Users\\sklee\\IdeaProjects")})
+
 public class InitParam extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("doPost");
@@ -31,9 +33,9 @@ public class InitParam extends HttpServlet {
         String pw = getInitParameter("pw");
         String path = getInitParameter("path");
 
-        System.out.println("id = "+id);
-        System.out.println("pw = "+pw);
-        System.out.println("path = "+path);
+        System.out.println("id = " + id);
+        System.out.println("pw = " + pw);
+        System.out.println("path = " + path);
 
         //response.setContentType("text/html; charset=UTF-8");
         /*response.setContentType("text/html; charset=EUC-KR");
