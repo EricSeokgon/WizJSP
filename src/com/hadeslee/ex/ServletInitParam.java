abd0ad4a421f8a884ae11c84a61c243939646389
hadaslee <leesk55@gmail.com>
2015-05-08 09:39:52 +0900

    servletinitparam 추가

---
+++ src/com/hadeslee/ex/ServletInitParam.java
@@ -0,0 +1,22 @@
+package com.hadeslee.ex;
+
+import java.io.IOException;
+
+/**
+ * Project: WizJSP
+ * FileName: ${NAME}
+ * Date: 2015-05-08
+ * Time: ¿ÀÀü 9:37
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class ServletInitParam extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
