915aad07b3cba835044c3975f75df626d03e63cf
hadaslee <leesk55@gmail.com>
2015-05-12 11:37:24 +0900

    frontcommand 패턴 테스트

---
+++ src/com/hadeslee/ex25/Service.java
@@ -0,0 +1,10 @@
+package com.hadeslee.ex25;
+
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface Service {
+	public ArrayList<MemberDto> execute(HttpServletRequest request, HttpServletResponse response);
+}
